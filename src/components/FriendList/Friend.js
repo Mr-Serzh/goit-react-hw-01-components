59bd3f25f392b19dc386b5911897a114e8de1dc2
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-11-28 03:59:08 +0200

    rezult task 3

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -1,22 +1,20 @@
 import PropTypes from 'prop-types';
 
+
 export default function Friend(props) {
-    const { avatar, name, isOnline, id } = props
+    const { avatar, name, isOnline } = props
     
     return (
-        <div>
-            <li key={id} className="item">
+        <>
             <span className="status">{isOnline}</span>
             <img className="avatar" src={avatar} alt={name} width="48" />
                 <p className="name">{name}</p>
-            </li>
-        </div>
+        </>
     )
     
 }
 
 Friend.propTypes = {
-    id: PropTypes.number,
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Friend from './Friend'
 
 export default function FriendList({friends}) {
@@ -6,31 +6,23 @@ export default function FriendList({friends}) {
     return (
         <ul className="friend-list">
             {friends.map(friend => (
-                // <li key={friend.id} className="item">
+                <li key={friend.id} className="item">
                 <Friend
-                    key={friend.id}
                     avatar={friend.avatar}
                     name={friend.name}
                     isOnline={friend.isOnline}
                 />
-                // </li>
+                </li>
             ))}
         </ul>
     );
 };
 
-// FriendList.propTypes = {
-//     friends: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//     }),
-//   ),
-// };
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
 
-// {/* <FriendList
-//     key={friends.id}
-//     avatar={friends.avatar}
-//     name={friends.name}
-//     isOnline={friends.isOnline}
-//     />
-//  */}
