46bf7ac148ab22bab9949e2ee18c574ba2adbbb5
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-12-01 02:05:42 +0200

    finished task 2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
-
+import s from './Statistics.module.css';
 
 export default function Statistics(props) {
-const { title, stats } = props;
+  const { title, stats } = props;
 
-    return (
-        <section className="statistics">
-        {title && <h2 className="title">{title}</h2>}
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
 
-        <ul className="stat-list">
-        {stats.map((stat) => (
-        <li key={stat.id} className="item">
-             <span className="label">{stat.label}</span>
-             <span className="percentage">{stat.percentage}%</span>
-        </li>
+      <ul className={s.statList}>
+        {stats.map(stat => (
+          <li key={stat.id} className={s.item}>
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}%</span>
+          </li>
         ))}
-        </ul>
-        </section>
-    )
-};
+      </ul>
+    </section>
+  );
+}
 
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
