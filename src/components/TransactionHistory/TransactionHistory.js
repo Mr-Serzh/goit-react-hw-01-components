39e5b43c07684c37a787da6bf15f66027049fc3d
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-12-01 02:06:06 +0200

    finished task 4

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,37 +1,39 @@
 import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
 
 export default function TransactionHistory(props) {
-    const { items } = props
-    
-    return (
-        <table className="transaction-history">
-        <thead>
-            <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-            </tr>
-        </thead>
+  const { items } = props;
 
-        <tbody>
-            {items.map((item) => (<tr key={item.id}>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
-            </tr>))}
-        </tbody>
-        </table>
-    )
-};
+  return (
+    <table className={s.transactionHistory}>
+      <thead>
+        <tr className={s.tableHead}>
+          <th className={s.item}>Type</th>
+          <th className={s.item}>Amount</th>
+          <th className={s.item}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id} className={s.row}>
+            <td className={s.item}>{item.type}</td>
+            <td className={s.item}>{item.amount}</td>
+            <td className={s.item}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        amount: PropTypes.string.isRequired,
-        currency: PropTypes.string.isRequired,
-        }),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
   ),
 };
-
