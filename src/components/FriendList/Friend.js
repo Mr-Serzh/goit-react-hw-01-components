c5ffc1bb813a71ef2e49f5fcf89857bf11951f94
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-12-04 03:47:39 +0200

    fix after mentor

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -5,11 +5,11 @@ export default function Friend(props) {
   const { avatar, name, isOnline } = props;
 
   return (
-    <>
+    <li className={s.item}>
       <span className={isOnline ? s.online : s.offline}>{isOnline}</span>
       <img className={s.avatar} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
-    </>
+    </li>
   );
 }
 
